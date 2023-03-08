c4f5690fa7747ffb409e00d5f1a8dcc22cbed7a8
Edric <edric.chan.1997@gmail.com>
2023-03-08 19:50:15 +0800

    ci: initial deploy workflow

---
+++ config-shared.js
@@ -0,0 +1,5 @@
+export const isCi = !!process.env.CI;
+
+export const repoName = process.env.GITHUB_REPOSITORY.split('/')[1] ?? 'mw3-playground';
+export const kitBasePath = isCi ? `/${repoName}` : '/';
+export const viteBasePath = isCi ? `/${repoName}/` : '/';
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteBasePath } from './config-shared';
 
 export default defineConfig({
+	base: viteBasePath,
 	plugins: [sveltekit()]
 });
