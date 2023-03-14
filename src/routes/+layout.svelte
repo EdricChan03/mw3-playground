0ef0590ec4e467a65aa7852f5448ba2c098689a0
Edric <edric.chan.1997@gmail.com>
2023-03-14 21:08:37 +0800

    fix: add missing Roboto font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import '../app.css';
 </script>
+
 <svelte:head>
   <title>Material 3 Playground</title>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
 </svelte:head>
 
 <slot />
