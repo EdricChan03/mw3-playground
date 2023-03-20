5e134723b5808fbdedc0f9dd64c97cc6f444e817
Edric <edric.chan.1997@gmail.com>
2023-03-21 02:11:15 +0800

    chore(Toc): remove active logic for now

--- src/lib/components/toc/Toc.svelte
+++ src/lib/components/toc/Toc.svelte
@@ -4,32 +4,6 @@
   import type { TocHeading } from './types';
 
   export let headings: TocHeading[];
-
-  const onLoad = () => {
-    console.log('svelte:window load');
-    const observer = new IntersectionObserver((entries) => {
-      console.log(entries);
-      entries.forEach((entry) => {
-        const id = entry.target.getAttribute('id');
-        console.log(`aside li a[href="#${id}"]`);
-        if (entry.intersectionRatio > 0) {
-          document.querySelector(`aside li a[href="#${id}"]`)!.parentElement!.classList.add('bg-blue-400', 'text-white');
-        } else {
-          document
-            .querySelector(`aside li a[href="#${id}"]`)!
-            .parentElement!.classList.remove('bg-blue-400', 'text-white');
-        }
-      });
-    });
-
-    // Track all sections that have an `id` applied
-    document.querySelectorAll('section + div[id]').forEach((section) => {
-      observer.observe(section);
-    });
-  };
-  onMount(() => {
-    onLoad();
-  })
 </script>
 
 <!-- <svelte:window on:load={onLoad} /> -->
