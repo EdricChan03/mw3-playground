187db573301077d3ff1434ab5822a74b225c33ed
Edric <edric.chan.1997@gmail.com>
2023-05-02 21:36:18 +0800

    style(svelte.config): reformat with 2 spaces

--- svelte.config.js
+++ svelte.config.js
@@ -9,17 +9,17 @@ const kitBasePath = isCi ? `/${repoName}` : '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: '404.html'
-		}),
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter({
+      fallback: '404.html'
+    }),
 
     prerender: {
       entries: ['*', ...components.map(c => `/components/${c.slug}`)]
@@ -29,10 +29,10 @@ const config = {
       '$examples/*': './src/lib/examples/*'
     },
 
-		paths: {
-			base: kitBasePath
-		}
-	},
+    paths: {
+      base: kitBasePath
+    }
+  },
 
 };
 
