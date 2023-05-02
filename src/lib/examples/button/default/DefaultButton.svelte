34fd5888b8ed22eb403e341bd1f125f64a954eb3
Edric <edric.chan.1997@gmail.com>
2023-05-02 22:07:32 +0800

    fix(ex/DefaultButton): document disabled FAB support
    See material-components/material-web#4045 for more info

--- src/lib/examples/button/default/DefaultButton.svelte
+++ src/lib/examples/button/default/DefaultButton.svelte
@@ -3,6 +3,25 @@
 </script>
 
 <div class="flex gap-3 flex-wrap items-center p-6">
+  <div
+    class="flex flex-col gap-3 p-3 border-2 border-red-500 bg-red-300/25 rounded shrink-0 basis-full"
+  >
+    <div class="flex gap-3 flex-1">
+      <md-icon class="basis-6 shrink-0" aria-hidden="true">error</md-icon>
+      <p>
+        From <a
+          href="https://github.com/material-components/material-web/releases/tag/v1.0.0-pre.5"
+          class="underline text-blue-700 hover:text-blue-800"><code>1.0.0-pre.5</code></a
+        >, <abbr title="Floating Action Button">FABs</abbr> no longer support specifying the
+        <code>disabled</code> attribute.
+      </p>
+    </div>
+    <md-outlined-button
+      trailingIcon
+      href="https://github.com/material-components/material-web/issues/4045"
+      class="self-end">Learn more<md-icon slot="icon">open_in_new</md-icon></md-outlined-button
+    >
+  </div>
   <md-elevated-button {disabled}>Elevated</md-elevated-button>
   <md-filled-button {disabled}>Filled</md-filled-button>
   <md-outlined-button {disabled}>Outlined</md-outlined-button>
